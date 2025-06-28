2849098f61807eb2cb27021c52c3debcd51d3e11
Vova Radionov <radionovdon@gmail.com>
2025-06-28 08:53:49 +0300

    modified hw9 test3 2part

--- hw9/test3.py
+++ hw9/test3.py
@@ -29,8 +29,11 @@ class StudentInfo:
     def __init__(self):
         self.students = []
 
-        self.student1 = Student(None, 'Aaa', 17, 4.0)
+        self.student1 = Student('Aa', 'Aaa', 17, 4.0)
         self.student2 = Student('Bb', 'Bbb', 18, 4.1)
+        self.student2 = Student('Cc', 'Ccc', 19, 3.7)
+        self.student2 = Student('Dd', 'Ddd', 20, 2.1)
+        self.student2 = Student('Ee', 'Eee', 21, 5.0)
 
         self.students.append(self.student1)
         self.students.append(self.student2)
@@ -53,7 +56,15 @@ class TestStudent(unittest.TestCase):
             self.assertIsNotNone(student.surname, "Surname cant be None")
             self.assertIsNotNone(student.age, "Age cant be None")
             self.assertIsNotNone(student.average_grade, "Grade cant be None")
-    
 
+            self.assertNotEqual(student.name, student.surname, "Name and surname cant be same")
+
+            self.assertGreater(5.01, student.average_grade, "Average_grade cant be greater than 5")
+            self.assertGreater(150, student.age, "Age cant be greater than 150")
+
+            self.assertIsInstance(student, Student, "student is an instance of class Student")
+
+            self.assertTrue(student.name.istitle(), "Name can be start from upper letter")
+            self.assertTrue(student.surname.istitle(), "Surname can be start from upper letter")
 
 unittest.main()
